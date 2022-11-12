5075a42b28598cfb769c6b3af73abc45d2bd9077
Daniel-17 <frimudaniel17@gmail.com>
2022-11-12 17:45:41 +0100

    parsing incoming requests

--- 2-express/app.js
+++ 2-express/app.js
@@ -1,16 +1,25 @@
-const http = require("http");
-
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>The add product page</h1>");
+  res.send(`
+    <form action="/product" method="post">
+      <input type="text" name='title' />
+      <button type="submit">add product</button>
+    </form>
+  `);
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In another middleware");
   res.send("<h1>Hello from express</h1>");
 });
 

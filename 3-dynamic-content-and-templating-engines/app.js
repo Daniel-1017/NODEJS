a9582e8d400c02873cb3767a05db919298ab0cff
Daniel-17 <frimudaniel17@gmail.com>
2022-11-13 14:20:25 +0100

    working on the layout with partials

--- 3-dynamic-content-and-templating-engines/app.js
+++ 3-dynamic-content-and-templating-engines/app.js
@@ -33,7 +33,9 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "404 page not found" });
+  res
+    .status(404)
+    .render("404", { pageTitle: "404 page not found", path: false });
 });
 
 app.listen(3000);

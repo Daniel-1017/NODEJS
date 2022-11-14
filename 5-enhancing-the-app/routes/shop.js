a85b2b877165a1f602efd5883f81cfa7e6b9b075
Daniel-17 <frimudaniel17@gmail.com>
2022-11-14 17:10:54 +0100

    orders item

--- 5-enhancing-the-app/routes/shop.js
+++ 5-enhancing-the-app/routes/shop.js
@@ -10,6 +10,8 @@ routes.get("/products", shopController.getProducts);
 
 routes.get("/cart", shopController.getCart);
 
+routes.get("/orders", shopController.getOrders);
+
 routes.get("/checkout", shopController.getCheckout);
 
 module.exports = routes;

c288e271d53ced490922b779d0743485b93fcf20
Daniel-17 <frimudaniel17@gmail.com>
2022-11-14 16:31:48 +0100

    enhancing the app - creating the shop structure

---
+++ 5-enhancing-the-app/routes/shop.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const productsController = require("../controllers/products");
+
+const routes = express.Router();
+
+routes.get("/", productsController.getProducts);
+
+module.exports = routes;

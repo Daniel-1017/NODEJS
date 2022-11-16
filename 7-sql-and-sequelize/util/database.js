87939a916dc5d7c6637d353d7ea1594142a84a74
Daniel-17 <frimudaniel17@gmail.com>
2022-11-16 18:04:32 +0100

    connecting to the database

--- 7-sql-and-sequelize/util/database.js
+++ 7-sql-and-sequelize/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "frimu_daniel17", {
+  dislect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "frimu_daniel17",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;

3ebb91759003d442fc6a3693ad91100019e801ea
Daniel-17 <frimudaniel17@gmail.com>
2022-11-16 18:14:33 +0100

    syncing the definition of the db

--- 7-sql-and-sequelize/util/database.js
+++ 7-sql-and-sequelize/util/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize("node-complete", "root", "frimu_daniel17", {
-  dislect: "mysql",
+  dialect: "mysql",
   host: "localhost",
 });
 

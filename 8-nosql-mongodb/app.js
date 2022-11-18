af00d5a827257b3d173c383924efa4de5bea5493
Daniel-17 <frimudaniel17@gmail.com>
2022-11-18 22:53:16 +0100

    using the db connection and adding products

--- 8-nosql-mongodb/app.js
+++ 8-nosql-mongodb/app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database");
+const mongoConnect = require("./util/database").mongoConnect;
 
 const app = express();
 
@@ -17,7 +17,9 @@ const adminRoutes = require("./routes/admin");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {});
+app.use((req, res, next) => {
+  next();
+});
 
 app.use("/admin", adminRoutes);
 // app.use(shopRoutes);

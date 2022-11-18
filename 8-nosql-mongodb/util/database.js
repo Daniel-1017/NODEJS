8981bea788cf66129b980fe7ae8303c23cc6ac4c
Daniel-17 <frimudaniel17@gmail.com>
2022-11-18 22:43:05 +0100

    creating database connection

--- 8-nosql-mongodb/util/database.js
+++ 8-nosql-mongodb/util/database.js
@@ -1,14 +1,26 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://first_user:1234@cluster0.j4grqal.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://first_user:1234@cluster0.j4grqal.mongodb.net/shop?retryWrites=true&w=majority"
   )
     .then((client) => {
-      callback(client);
+      _db = client.db();
+      callback();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) return _db;
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;

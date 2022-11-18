8a76690246d4fa65318c2e752f3704c63df052e8
Daniel-17 <frimudaniel17@gmail.com>
2022-11-19 00:38:16 +0100

    cart items and add to cart functionallity

--- 8-nosql-mongodb/app.js
+++ 8-nosql-mongodb/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findUserById("6378119d05f4aa62b9bc08b8")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));

2a408548f1aed1f25a7bd76ca9b7b3a44148cb05
Daniel-17 <frimudaniel17@gmail.com>
2022-11-29 20:40:55 +0100

    sending and handling background requests

--- 17-async-requests/controllers/admin.js
+++ 17-async-requests/controllers/admin.js
@@ -168,8 +168,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then((product) => {
       if (!product) {
@@ -180,11 +180,9 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .then(() => {
       console.log("DESTROYED PRODUCT");
-      res.redirect("/admin/products");
+      res.status(200).json({ message: "success" });
     })
     .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ message: "deleting product faild" });
     });
 };

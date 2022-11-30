f40e462f640f7bc8bef4daa35edfd3e36469de1f
Daniel-17 <frimudaniel17@gmail.com>
2022-11-30 17:03:08 +0100

    sending requests and responses

--- 19-rest-apis/controllers/feed.js
+++ 19-rest-apis/controllers/feed.js
@@ -1,3 +1,21 @@
 exports.getPosts = (req, res, next) => {
-  // to do
+  res.status(200).json({
+    posts: [
+      {
+        title: "First Post",
+        content:
+          "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, unde.",
+      },
+    ],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // CREATE POST IN DB
+  res.status(201).json({
+    message: "POST CREATED SUCCESSFULLY!",
+    post: { id: Math.random(), title, content },
+  });
 };

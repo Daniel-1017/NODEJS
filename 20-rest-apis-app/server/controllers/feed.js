881a9c285e054b0cf1618e06e43d0ece60d51099
Daniel-17 <frimudaniel17@gmail.com>
2022-11-30 17:44:47 +0100

    create add post endpoint

--- 20-rest-apis-app/server/controllers/feed.js
+++ 20-rest-apis-app/server/controllers/feed.js
@@ -2,16 +2,16 @@ exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
-        _id: '1',
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/duck.jpg',
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/boat.jpg",
         creator: {
-          name: 'Maximilian'
+          name: "Daniel",
         },
-        createdAt: new Date()
-      }
-    ]
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
@@ -20,7 +20,13 @@ exports.createPost = (req, res, next) => {
   const content = req.body.content;
   // Create post in db
   res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    message: "Post created successfully!",
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "Daniel" },
+      createdAt: new Date(),
+    },
   });
 };

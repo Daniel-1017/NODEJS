b68b33889768c98c231d804d016a41cde3bcda22
Daniel-17 <frimudaniel17@gmail.com>
2022-11-30 18:27:26 +0100

    storing posts in the database

--- 20-rest-apis-app/server/controllers/feed.js
+++ 20-rest-apis-app/server/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require("express-validator/check");
 
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -21,26 +23,29 @@ exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: "Validation failed, entered data is incorect.",
-        errors: errors.array(),
-      });
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorect.",
+      errors: errors.array(),
+    });
   }
 
   const title = req.body.title;
   const content = req.body.content;
 
-  // Create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: { name: "Daniel" },
-      createdAt: new Date(),
-    },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/boat.jpg",
+    creator: { name: "Daniel" },
   });
+  post
+    .save()
+    .then((resutl) => {
+      console.log(resutl);
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: resutl,
+      });
+    })
+    .catch((err) => console.log(err));
 };
